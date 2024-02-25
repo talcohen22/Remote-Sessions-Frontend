d21ea854870457dd9d09a31e2305bc89c3332e1c
Tal Cohen <talco4088@gmail.com>
2024-02-25 15:34:16 +0200

    fixed the sockets- the first client who enters each of the code block- he is the teacher and have read only mode

--- src/cmps/CodeDetails.jsx
+++ src/cmps/CodeDetails.jsx
@@ -9,30 +9,36 @@ import { CodeEditor } from "./CodeEditor";
 const baseUrl = (process.env.NODE_ENV === 'production') ? '//remote-sessions-1vt0.onrender.com' : '//localhost:3030'
 const socket = io.connect(baseUrl)
 
-
-
 export function CodeDetails() {
 
     const { codeId } = useParams()
     const [code, setCode] = useState('')
     const [firstClientConnected, setFirstClientConnected] = useState('')
     const [isShowMessage, seIsShowMessage] = useState(false)
-    const timeoutId = useRef()
+    const timeoutErrMsgId = useRef()
+    const debounceTimeoutId = useRef()
     const navigate = useNavigate()
 
-
     useEffect(() => {
+
+        loadCode()
+
+        socket.emit('client connected', codeId) //emit to the backend that a client connected
+        
+        socket.on('set first client', firstClientConnected => { //get the first client connected from the backend
+            setFirstClientConnected(firstClientConnected)
+        })
+        
         socket.on('code updated', (code) => { //listening to a 'code update' event when this cmp goes up
             setCode(code)
         })
 
-        socket.emit('client connected') //emit to the backend that a client connected
-        socket.on('set first client', firstClientConnected => { //get the first client connected from the backend
-            setFirstClientConnected(firstClientConnected)
-        })
+        return () => {
+            socket.emit('client leave room', codeId) 
+        };
+        
 
-        loadCode()
-    }, [codeId])
+    }, [])
 
     async function loadCode() { //find the relevant code from db
         try {
@@ -46,27 +52,40 @@ export function CodeDetails() {
     async function onHandleChange(value) {
         try {
             if (firstClientConnected === socket.id) { //the first client connected (the teacher) cannot to edit the code- a message will show
-                seIsShowMessage(true)
-                clearTimeout(timeoutId.current)
-                timeoutId.current = setTimeout(() => { //the message present for 3 seconds from the moment he started to type
-                    seIsShowMessage(false)
-                }, 3000);
+                showErrorMsg()
             }
             else { //update the code state and the db
-                const newCode = { ...code, content: value }
-                const updateCode = await codeService.edit(newCode)
-                setCode(updateCode)
-
-                socket.emit('code changed', updateCode) //emit to the backend that the event 'code changed' happened
+                updateCode(value)
             }
         } catch (err) {
             console.log('Cannot update code', err);
         }
     }
 
+    function showErrorMsg() {
+        seIsShowMessage(true)
+        clearTimeout(timeoutErrMsgId.current)
+        timeoutErrMsgId.current = setTimeout(() => { //the message present for 3 seconds from the moment he started to type
+            seIsShowMessage(false)
+        }, 3000);
+    }
+
+    function updateCode(value) {
+        const newCode = { ...code, content: value }
+        setCode(newCode)
+        clearTimeout(debounceTimeoutId.current)
+        debounceTimeoutId.current = setTimeout(async () => { //save the new code in db just after 1 seconds that the user is not typing
+            const updateCode = await codeService.edit(newCode)
+            socket.emit('code changed', updateCode) //emit to the backend that the event 'code changed' happened
+        }, 1000);
+    }
+
     function getLobbyPage() { //When the user click on the back button, he will redirected to the lobby page
         navigate('/')
     }
+
+
+    const correctAns = code.content && code.solution && code.content.replace(/\s/g, '') === code.solution.replace(/\s/g, '')
 
     return (
         <section className="code-details">
@@ -80,13 +99,10 @@ export function CodeDetails() {
 
             <p className="error-message">{isShowMessage ? `You are in read mode, your changes will not be saved` : ''}</p>
 
-            {code.content && code.solution &&
-                code.content.replace(/\s/g, '') === code.solution.replace(/\s/g, '') &&
-                <p className="solution">🤩💪</p>}
+            {correctAns && <p className="solution">🤩💪</p>}
 
             <CodeEditor code={code} onHandleChange={onHandleChange} />
 
         </section>
     )
 }
-
